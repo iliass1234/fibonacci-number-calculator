81c500249dad319b027f47c99ae3f177d3095d79
ilias jabri <68255254+iliass1234@users.noreply.github.com>
2022-01-22 02:57:58 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,6 @@
 let input = document.querySelector('#input')
 let enter = document.querySelector('#button')
-
-
+let place = document.querySelector('#place-results')
 
 let map = new Map
 let fibo;
@@ -16,15 +15,40 @@ function fib(n){
     return fibo
 }
 
-enter.onclick = function(){
+function fixResultBig(res){
+    let str = res.toString()
+    if (str.length>10) {
+        str = str.split('')
+        str = str.slice(0,10)
+        str.push('...')
+        str = str.join('')
+    } 
+    return str
+}
+
+function addResult(){
+    let val = input.value
     if (input.value == '') {
         return
     }
-    fib(parseInt(input.value))
-    input.value = fib(parseInt(input.value))
-    input.placeholder = fib(parseInt(input.value))
-
+    input.value = fib(parseInt(val))
+    let divo = document.createElement('div')
+    let divoSpan1 = document.createElement('span')
+    let divoSpan2 = document.createElement('span')
+    divoSpan1.className = 'span1'
+    divoSpan1.innerText = val
+    divoSpan2.innerText = fixResultBig(fib(parseInt(val)))
+    divo.className = 'one-result'
+    divo.append(divoSpan1,' : ',divoSpan2)
+    place.appendChild(divo)
 }
+//----------------------------------------------------
+let j = fib(parseInt(12))
+console.log(fixResultBig(123456789101112))
+enter.onclick = addResult
 
-
-console.log(fib(200))
+input.addEventListener('keydown',(event)=>{
+    if (event.keyCode == 13 && input.value !== '' && parseInt(input.value)/2) {
+       addResult()
+    }
+})
